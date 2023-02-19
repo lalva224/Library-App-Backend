b7ffe23f2f910b958ef9aceae9826c9c11467d33
doapo <124337121+doapo@users.noreply.github.com>
2023-02-19 02:44:50 -0500

    test
    test

--- mysqlDemo/src/main/java/com/example/mysqlDemo/User.java
+++ mysqlDemo/src/main/java/com/example/mysqlDemo/User.java
@@ -5,15 +5,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+
+//import lombok.Data;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name = "User")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
+    @Column(name = "name")
     private String name;
-
+    @Column(name = "email")
     private String email;
 
     public Integer getId() {
