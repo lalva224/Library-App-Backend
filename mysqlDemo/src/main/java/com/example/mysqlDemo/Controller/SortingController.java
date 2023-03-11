3d7027a509ec0089fce31c6fa33f87e5871fb681
Jairo Alvarenga <jairoalvarenga2016@gmail.com>
2023-03-10 23:04:13 -0500

    RetrieveByGenre, Retrieve top sellers & rating

--- mysqlDemo/src/main/java/com/example/mysqlDemo/Controller/SortingController.java
+++ mysqlDemo/src/main/java/com/example/mysqlDemo/Controller/SortingController.java
@@ -1,5 +1,9 @@
 package com.example.mysqlDemo.Controller;
 
+import java.util.concurrent.Future;
+import java.util.List;
+
+import org.hibernate.sql.exec.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +15,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.mysqlDemo.Modals.Book;
 import com.example.mysqlDemo.Service.BookSortingRepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping(path="/sorting")
-public class SortingController {
+public class SortingController<E> {
     @Autowired
     private BookSortingRepository bookRepo;
 
     @PostMapping(path="/add") // Map ONLY POST Requests
     public @ResponseBody String addNewBook (@RequestParam String isbn, @RequestParam String name,
-    @RequestParam String description, @RequestParam double price, @RequestParam String authorName, @RequestParam String genre, @RequestParam String publisher, @RequestParam int yearPublished, @RequestParam int copiesSold) {
+    @RequestParam String description, @RequestParam double price, @RequestParam String authorName, @RequestParam String genre, @RequestParam String publisher, @RequestParam int yearPublished, @RequestParam int copiesSold, @RequestParam double rating) {
         
 
         Book book = new Book();
@@ -32,18 +44,39 @@ public class SortingController {
         book.setPublisher(publisher);
         book.setYearPublished(yearPublished);
         book.setCopiesSold(copiesSold);
+        book.setRating(rating);
         bookRepo.save(book);
+        return "Book has been Saved";
+    }
+
+    @GetMapping(path="/getByGenre")
+    public  @ResponseBody  Future<List<Book>> getByGenre( @RequestParam(name = "bookGenre") String bookGenre) {  
+      Future<List<Book>> booksByGenre = bookRepo.findByGenre(bookGenre);
+      return booksByGenre;
+    }
+
+    @GetMapping(path="/topSellers")
+    public  @ResponseBody  Future<List<Book>> topSellers() {  
+      Future<List<Book>> booksByTopSellers = bookRepo.topSellers();
+      return booksByTopSellers;
+    }
+
+    @GetMapping(path="/rating")
+    public  @ResponseBody  Future<List<Book>> getByRating(@RequestParam(name = "rating") double rating) {  
+      Future<List<Book>> booksByRating = bookRepo.getByRating(rating);
+      return booksByRating;
+    }
+
+    @PostMapping(path="/discountByPublisher")
+    public  @ResponseBody String discountByPublisher(@RequestParam(name = "publisher") String publisher, @RequestParam(name = "percentDiscount") double percentDiscount) {  
+      
+        Future<List<Book>> getPublisherBooks = bookRepo.getPublisherBooks(publisher);
+        getPublisherBooks.
+
         return "Saved";
 
 
-        // User n = new User();
-        // n.setUsername(username);
-        // n.setPassword(password);
-        // n.setName(name);
-        // n.setEmail(email);
-        // n.setHomeAddress(homeAddress);
-        // userRepository.save(n);
-        // return "Saved";
+
     }
 
     @GetMapping(path="/all")
